1f1317be6735eafb169220066bbcb6d9d4f12438
Zimu.Liu <Zimu.Liu@e081ab05-bb53-0410-b40c-8f1e9d9e73b3>
2008-08-05 23:32:09 +0000

    kernel controlling functions

--- trunk/drcomd/kmodule.c
+++ trunk/drcomd/kmodule.c
@@ -2,6 +2,11 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/kern_control.h>
+#include <sys/sys_domain.h>
 
 #include "drcomd.h"
 #include "daemon_kernel.h"
@@ -10,89 +15,137 @@
 
 int module_start_auth(struct drcom_handle *h)
 {
-/*
-	int sock;
+	int retval = 0;
+	int ctl_fd = 0;
+	struct ctl_info ctl_info;
+	struct sockaddr_ctl sc;
+	
 	struct drcom_conf *conf = (struct drcom_conf*)h->conf;
 	struct drcom_session_info *s;
-	struct conn_param *cp;
-        struct conn_auth_cmd cmd;
-	int len, ret;
-
-	sock = socket(PF_INET, SOCK_DGRAM, 0);
-	if (sock < 0){
-		logerr("socket create failure\n");
+	struct drcom_set_params_opt * drcom_params_opt_ptr;
+	struct drcom_set_auth_opt drcom_auth_opt;
+	size_t len;
+	
+	ctl_fd = socket(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL);
+	if (ctl_fd < 0){
+		logerr("socket(): %s, errno = %d\n", strerror(errno), errno);
 		return -1;
 	}
-
-	len = sizeof(struct e_address)*conf->except_count+sizeof(struct conn_param);
-	cp = (struct conn_param *)malloc(len);
-	if (cp==NULL){
-		logerr("malloc failure\n");
-		close(sock);
+	
+	bzero(&ctl_info, sizeof(struct ctl_info));
+	strcpy(ctl_info.ctl_name, MYBUNDLEID);
+	retval = ioctl(ctl_fd, CTLIOCGINFO, &ctl_info);
+	if (retval < 0) {
+		logerr("ioctl(CTLIOCGINFO): %s, errno = %d\n", strerror(errno), errno);
 		return -1;
 	}
-
-	strncpy(cp->devname, conf->device, IFNAMSIZ);
-	cp->e_count = conf->except_count;
-	memcpy(cp->es, conf->except, cp->e_count*sizeof(struct e_address));
-
-        ret = setsockopt(sock, IPPROTO_IP, CONN_SO_SET_PARAMS, cp, len);
-        if (ret != 0) {
-                logerr("setsockopt(CONN_SO_SET_PARAMS) failed\n");
-		close(sock);
-		free(cp);
-                return -1;
-        }
-
-	free(cp);
-
+	
+	bzero(&sc, sizeof(struct sockaddr_ctl));
+	sc.sc_len = sizeof(struct sockaddr_ctl);
+	sc.sc_family = AF_SYSTEM;
+	sc.ss_sysaddr = SYSPROTO_CONTROL;
+	sc.sc_id = ctl_info.ctl_id;
+	sc.sc_unit = 0;
+	retval = connect(ctl_fd, (struct sockaddr *) &sc, sizeof(struct sockaddr_ctl));
+	if (retval < 0)
+	{
+		logerr("connect(): %s, errno = %d\n", strerror(errno), errno);
+		close(ctl_fd);
+		return -1;
+	}
+	
+	len = sizeof(struct exclude_entry)*conf->except_count + sizeof(struct drcom_set_params_opt);
+	drcom_params_opt_ptr = (struct drcom_set_params_opt *)malloc(len);
+	if (NULL == drcom_params_opt_ptr)
+	{
+		logerr("malloc() failed\n");
+		close(ctl_fd);
+		return -1;
+	}
+	
+	drcom_params_opt_ptr->exclude_count = conf->except_count;
+	memcpy(drcom_params_opt_ptr->exclude_list,
+		conf->except,
+		drcom_params_opt_ptr->exclude_count * sizeof(struct exclude_entry));
+	
+	retval = setsockopt(ctl_fd, SYSPROTO_CONTROL, DRCOM_CTL_PARAMS, drcom_params_opt_ptr, len);
+	if (retval != 0) {
+		logerr("setsockopt(DRCOM_CTL_PARAMS): %s, errno = %d\n", strerror(errno), errno);
+		close(ctl_fd);
+		free(drcom_params_opt_ptr);
+		return -1;
+	}	
+	free(drcom_params_opt_ptr);
+	
 	s = drcom_get_session_info(h);
-
-        cmd.cmd = CONN_MODE_AUTH;
-	cmd.pid = getpid();
-	cmd.autologout = conf->autologout;
-	memcpy(cmd.auth_data, s->auth, sizeof(struct drcom_auth));
-
-        ret = setsockopt(sock, IPPROTO_IP, CONN_SO_SET_AUTH_CMD, &cmd, sizeof(struct conn_auth_cmd));
-        if (ret != 0) {
-                logerr("CONN_SO_SET_AUTH_CMD failed\n");
-		close(sock);
-                return -1;
-        }
-
-	close(sock);
-
+	
+	drcom_auth_opt.cmd = DRCOM_AUTH_MODE_ON;
+	drcom_auth_opt.pid = getpid();
+	drcom_auth_opt.autologout = conf->autologout;
+	memcpy(drcom_auth_opt.auth_data, s->auth, sizeof(struct drcom_auth));
+	
+	retval = setsockopt(ctl_fd, SYSPROTO_CONTROL, DRCOM_CTL_AUTH, &drcom_auth_opt, sizeof(drcom_auth_opt));
+	if (retval != 0) {
+		logerr("setsockopt(DRCOM_CTL_AUTH): %s, errno = %d\n", strerror(errno), errno);
+		close(ctl_fd);
+		return -1;
+	}
+	
+	close(ctl_fd);
+	
 	loginfo("daemon: Starting authentication...\n");
-*/
-	return 0;
-
+	return 0;	
 }
 
 int module_stop_auth(void)
 {
-/*
-	int sock;
-        struct conn_auth_cmd cmd;
-	int ret;
-
-	sock = socket(PF_INET, SOCK_DGRAM, 0);
-	if (sock < 0){
-		logerr("socket create failure\n");
+	int retval = 0;
+	int ctl_fd = 0;
+	struct ctl_info ctl_info;
+	struct sockaddr_ctl sc;
+	
+	struct drcom_set_auth_opt drcom_auth_opt;
+	
+	ctl_fd = socket(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL);
+	if (ctl_fd < 0){
+		logerr("socket(): %s, errno = %d\n", strerror(errno), errno);
 		return -1;
 	}
-
-	cmd.cmd = CONN_MODE_NONE;
-        ret = setsockopt(sock, IPPROTO_IP, CONN_SO_SET_AUTH_CMD, &cmd, sizeof(struct conn_auth_cmd));
-        if (ret != 0) {
-                logerr("CONN_SO_SET_AUTH_CMD failed\n");
-		close(sock);
-                return -1;
-        }
-
-	close(sock);
+	
+	bzero(&ctl_info, sizeof(struct ctl_info));
+	strcpy(ctl_info.ctl_name, MYBUNDLEID);
+	retval = ioctl(ctl_fd, CTLIOCGINFO, &ctl_info);
+	if (retval < 0) {
+		logerr("ioctl(CTLIOCGINFO): %s, errno = %d\n", strerror(errno), errno);
+		return -1;
+	}
+	
+	bzero(&sc, sizeof(struct sockaddr_ctl));
+	sc.sc_len = sizeof(struct sockaddr_ctl);
+	sc.sc_family = AF_SYSTEM;
+	sc.ss_sysaddr = SYSPROTO_CONTROL;
+	sc.sc_id = ctl_info.ctl_id;
+	sc.sc_unit = 0;
+	retval = connect(ctl_fd, (struct sockaddr *) &sc, sizeof(struct sockaddr_ctl));
+	if (retval < 0)
+	{
+		logerr("connect(): %s, errno = %d\n", strerror(errno), errno);
+		close(ctl_fd);
+		return -1;
+	}
+		
+	drcom_auth_opt.cmd = DRCOM_AUTH_MODE_OFF;
+	
+	retval = setsockopt(ctl_fd, SYSPROTO_CONTROL, DRCOM_CTL_AUTH, &drcom_auth_opt, sizeof(drcom_auth_opt));
+	if (retval != 0) {
+		logerr("setsockopt(DRCOM_CTL_AUTH): %s, errno = %d\n", strerror(errno), errno);
+		close(ctl_fd);
+		return -1;
+	}
+	
+	close(ctl_fd);
 
 	loginfo("daemon: Stopping authentication...\n");
-*/
 	return -1;
 }
 
